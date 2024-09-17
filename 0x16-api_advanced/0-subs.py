207aba4e373b9e19f1b2782d44fca25fceffaf23
George <ndungugeorge910@gmail.com>
2024-09-17 12:57:00 -0400

    subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "requests"
+        "User-Agent": "python:subreddit.subscriber.count:v1.0"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
